bdb89e816751022852419cf429dbb46ea5506b2e
knot5 <147835176+knot5@users.noreply.github.com>
2024-01-10 22:11:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const svg = document.getElementById('triangles');
+
+svg.onmouseenter = (e) => {
+    const colors = ['red', 'blue', 'green', 'orange', 'pink', 'purple'];
+    const rando = () => colors[Math.floor(Math.random() * colors.length)];
+
+    document.documentElement.style.cssText = `
+    
+    --dark-color: ${rando()};
+    --light-color: ${rando()};
+    `
+}
