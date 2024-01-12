f1096d1324fb35be37e2b032a4cf1e3084dd6a11
knot5 <147835176+knot5@users.noreply.github.com>
2024-01-12 22:09:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ svg.onmouseenter = (e) => {
     --light-color: ${rando()};
     `
 }
+
+
+const decreaseBtn = document.getElementById('decreaseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const increaseBtn = document.getElementById('increaseBtn');
+
+
+let count = 0;
+
+increaseBtn.onclick = function() {
+  count++;
+  countLabel.textContent = count
+}
+
+decreaseBtn.onclick = function() {
+  count--;
+  countLabel.textContent = count
+}
+
+resetBtn.onclick = function() {
+  count = 0
+  countLabel.textContent = count;
+}
