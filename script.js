9ab99c2fbd08d57ad68563b95f72ef190ea18f73
knot5 <147835176+knot5@users.noreply.github.com>
2024-03-13 15:39:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+document.getElementById('siteContent');
+
+function showSite(){
+  siteContent.style.display = 'block'
+
+}
+
+document.getElementById('siteContent');
+
+function hideSite(){
+  siteContent.style.display = 'none'
+}
+
 const svg = document.getElementById('triangles');
 
 svg.onmouseenter = (e) => {
@@ -67,3 +81,126 @@ function rollDice(){
   diceImages.innerHTML = images.join('')
 
 }
+
+
+
+const boxRezultat = document.getElementById('boxRezultat')
+const buttonNu = document.getElementById('buttonNu');
+const buttonDa = document.getElementById('buttonDa');
+const newP = document.createElement('p');
+
+
+buttonDa.addEventListener('click', event => {
+  newP.setAttribute('id','newP');
+  newP.textContent = 'Da..'
+  newP.style.fontSize = '2rem'
+  boxRezultat.appendChild(newP);
+});
+
+
+buttonNu.addEventListener('mouseover', event => {
+    event.target.textContent = "Da"
+    event.target.style.backgroundColor = 'lightgreen'
+});
+
+buttonNu.addEventListener('mouseout', event => {
+    event.target.textContent = "Nu"
+    event.target.style.backgroundColor = 'tomato'
+
+})
+
+buttonNu.addEventListener('click', event => {
+  newP.setAttribute('id','newP');
+  newP.textContent = 'Ia mai gandeste-te'
+  newP.style.fontSize = '2rem'
+  boxRezultat.appendChild(newP);
+});
+
+
+
+
+buttonReset.addEventListener('click', event => {
+    
+  boxRezultat.removeChild(newP);
+});
+
+
+const calculateBtn = document.getElementById('myBtn');
+const resultLabel = document.getElementById('myLabel');
+
+console.log(typeof a);
+console.log(typeof b);
+
+
+
+function calculate(){
+   
+
+    try {
+        const a = Number(document.getElementById('myInput1').value);
+        const b = Number(document.getElementById('myInput2').value);
+        let result = 0;
+        result = result + (a / b);
+        resultLabel.textContent = result;
+        console.log(a);
+        console.log(b);
+        console.log(result);
+
+        if(b == 0){
+            resultLabel.textContent = `Cannot divide by 0`;
+            throw new Error(`Cannot divide by 0`) 
+        }
+
+        if(isNaN(a) || isNaN(b)){
+            resultLabel.textContent = `No Numbers`;
+            throw new Error(`No Numbers`) 
+        }
+
+    }
+
+    catch(error){
+        console.error(error)
+    };
+
+};
+
+function clearResult(){
+    resultLabel.textContent = '0';
+    
+}
+
+function addColor(){
+    const body = document.getElementById('body');
+    body.classList.add('colorful')
+}
+
+function removeColor(){
+    const body = document.getElementById('body');
+    body.classList.remove('colorful')
+}
+
+function pauseColor(){
+    const body = document.getElementById('body');
+    body.classList.add('pauseColor')
+}
+
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+    display.value += input;
+};
+
+function clearDisplay(){
+    display.value = '0';
+};
+
+function calculate(){
+    try{
+        display.value = eval(display.value)
+    }
+    catch(error){
+        display.value = 'error';
+    }
+    
+};
